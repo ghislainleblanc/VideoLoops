fdb1135775cdb6be541f4104dd147bfbb4dabcd0
Ghislain Leblanc <ghisleb@me.com>
2019-02-23 09:53:49 -0500

    Can play full video with taping gesture.

--- VideoLoops/VideoLoops/VideoViewController.swift
+++ VideoLoops/VideoLoops/VideoViewController.swift
@@ -7,33 +7,60 @@
 //
 
 import UIKit
-import AVKit
+import AVFoundation
 
-struct VideoViewControllerViewModel {
-    var filename: String
-}
-
-class VideoViewController: UIViewController {
-    var viewModel: VideoViewControllerViewModel?
-
+class VideoViewControllerViewModel {
     private var player: AVQueuePlayer!
     private var playerLayer: AVPlayerLayer!
     private var playerItem: AVPlayerItem!
     private var playerLooper: AVPlayerLooper!
+    private var filename: String
 
-    override func viewDidLoad() {
-        let path = Bundle.main.path(forResource: viewModel?.filename, ofType: "mp4")
+    var viewLayer: AVPlayerLayer {
+        let path = Bundle.main.path(forResource: filename, ofType: "mp4")
         let pathURL = URL(fileURLWithPath: path!)
 
         player = AVQueuePlayer()
         playerLayer = AVPlayerLayer(player: player)
         playerItem = AVPlayerItem(url: pathURL)
-        playerLooper = AVPlayerLooper(player: player, templateItem: playerItem,
-                                      timeRange: CMTimeRange(start: CMTime.zero, end: CMTimeMake(value: 6, timescale: 1)) )
         playerLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
-        playerLayer.frame = view.layer.bounds
-        view.layer.insertSublayer(playerLayer, at: 1)
+
+        return playerLayer
+    }
+
+    init(filename: String) {
+        self.filename = filename
+    }
+
+    func playIntro() {
+        player.removeAllItems()
+        let range = CMTimeRange(start: CMTime.zero, end: CMTimeMake(value: 6, timescale: 1))
+        playerLooper = AVPlayerLooper(player: player, templateItem: playerItem, timeRange: range)
+        player.play()
+    }
+
+    func playFull() {
+        player.removeAllItems()
+        let range = CMTimeRange(start: CMTime.zero, end: playerItem!.asset.duration)
+        playerLooper = AVPlayerLooper(player: player, templateItem: playerItem, timeRange: range)
 
         player.play()
+    }
+}
+
+class VideoViewController: UIViewController {
+    var viewModel: VideoViewControllerViewModel?
+
+    override func viewDidLoad() {
+        if let layer = viewModel?.viewLayer {
+            layer.frame = view.layer.frame
+            view.layer.insertSublayer(layer, at: 1)
+
+            viewModel?.playIntro()
+        }
+    }
+
+    @IBAction private func didTap() {
+        viewModel?.playFull()
     }
 }
