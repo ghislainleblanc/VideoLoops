2c00069d0930d6b4fd21df09418ee04aabe9bf32
Ghislain Leblanc <ghisleb@me.com>
2019-02-23 03:52:25 -0500

    Empty project.

---
+++ VideoLoops/VideoLoops/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoLoops
+//
+//  Created by Ghislain Leblanc on 2019-02-23.
+//  Copyright © 2019 Ghislain Leblanc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
